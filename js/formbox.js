a1ad756055bcb770b849deabac215308073aac89
Vincent <41871909+vincentop@users.noreply.github.com>
2020-10-30 15:48:40 +0800

    update1030v1

--- js/formbox.js
+++ js/formbox.js
@@ -1,6 +1,4 @@
 //input type=date 日期的提示清除
-
-window.onload = function(){
 $("input[name='bookingTime']").on("input",function(){
     if($(this).val().length>0){
         $(this).addClass("full");
@@ -8,4 +6,3 @@ $("input[name='bookingTime']").on("input",function(){
         $(this).removeClass("full");
     }
 });
-}
---
+++ js/scrollPage.js
@@ -0,0 +1,39 @@
+$("#cater1").click(function(){
+	var pos = $("#ourService").position().top;
+	$("html,body").stop(true,false).animate({scrollTop:pos-102},500, "easeOutBack");
+});
+	
+$("#cater2").click(function(){
+	var pos = $("#ourService").position().top;
+	$("html,body").stop(true,false).animate({scrollTop:pos-102},500, "easeOutBack");
+});
+	
+$("#cater3").click(function(){
+	var pos = $("#ourService").position().top;
+	$("html,body").stop(true,false).animate({scrollTop:pos-102},500, "easeOutBack");	
+});
+	
+$("#cater4").click(function(){
+	var pos = $("#aboutUs").position().top;
+	$("html,body").stop(true,false).animate({scrollTop:pos-102},500, "easeOutBack");	
+});
+
+$("#cater5").click(function(){
+	var pos = $("#catPhotos").position().top;
+	$("html,body").stop(true,false).animate({scrollTop:pos-102},500, "easeOutBack");	
+});
+
+$("#cater6").click(function(){
+	var pos = $("#onlineBooking").position().top;
+	$("html,body").stop(true,false).animate({scrollTop:pos-102},500, "easeOutBack");	
+});
+
+$("#cater7").click(function(){
+	var pos = $("#contactUs").position().top;
+	$("html,body").stop(true,false).animate({scrollTop:pos-102},500, "easeOutBack");	
+});
+
+$("#cater8").click(function(){
+	var pos = $("#ranking").position().top;
+	$("html,body").stop(true,false).animate({scrollTop:pos-102},500, "easeOutBack");	
+});
