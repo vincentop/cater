e10d2443192a814b43a49edf77aed6ce45b727cd
Vincent <41871909+vincentop@users.noreply.github.com>
2020-11-02 14:33:13 +0800

    update form

--- js/formbox.js
+++ js/formbox.js
@@ -1,9 +1,9 @@
 //input type=date 日期的提示清除
+
 $("input[type='datetime-local']").on("input",function(){
-    if($(this).val().length<=0){
-        $(this).addClass("empty");
-    }else{
-        // $(this).removeClass();
+    if($(this).val().length>0){
         $(this).addClass("full");
+    }else{
+        $(this).removeClass("full");
     }
 });
