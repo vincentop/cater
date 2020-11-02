f9a106d700413faaf5a9c733cac0f977349e8e9a
Vincent <41871909+vincentop@users.noreply.github.com>
2020-11-02 13:50:36 +0800

    update form

--- js/formbox.js
+++ js/formbox.js
@@ -1,8 +1,9 @@
 //input type=date 日期的提示清除
 $("input[name='bookingTime']").on("input",function(){
-    if($(this).val().length>0){
-        $(this).addClass("full");
+    if($(this).val().length<=0){
+        $(this).addClass("empty");
     }else{
-        $(this).removeClass();
+        // $(this).removeClass();
+        $(this).addClass("full");
     }
 });
